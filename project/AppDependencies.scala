10988b49eea5b54c471d3b3e3a1527dd63ea8df5
Phil Evans <65957825+hmrc-phil@users.noreply.github.com>
2021-11-15 16:14:05 +0000

    BDOG-1575 bump versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,19 +3,22 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "5.16.0"
+  val hmrcMongoVersion = "0.56.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"             %% "govuk-template"           % "5.60.0-play-26",
-    "uk.gov.hmrc"             %% "play-ui"                  % "8.18.0-play-26",
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "2.2.0",
-    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.31.0-play-26"
+    "uk.gov.hmrc"             %% "govuk-template"              % "5.72.0-play-28",
+    "uk.gov.hmrc"             %% "play-ui"                     % "9.7.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"          % hmrcMongoVersion
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % Test,
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % Test,
-    "com.typesafe.play"       %% "play-test"                % PlayVersion.current     % Test,
-    "org.pegdown"              % "pegdown"                  % "1.6.0"                 % Test,
-    "uk.gov.hmrc"             %% "reactivemongo-test"       % "4.22.0-play-26"        % Test
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"      % bootstrapVersion        % Test,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"     % hmrcMongoVersion        % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"          % "4.0.3"                 % Test,
+    "com.typesafe.play"       %% "play-test"                   % PlayVersion.current     % Test,
+    "org.pegdown"              % "pegdown"                     % "1.6.0"                 % Test,
   )
 
 }
