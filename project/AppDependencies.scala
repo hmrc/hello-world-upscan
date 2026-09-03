931c07f794eabb7225beabb48e7fd429167b0cc1
ibrahimaziz11 <312463139+ibrahimaziz11@users.noreply.github.com>
2026-09-03 14:24:17 +0100

    Bumped play-frontend library to version 13.13.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"  % "12.32.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"  % "13.13.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"          % hmrcMongoVersion,
     "uk.gov.hmrc.objectstore" %% "object-store-client-play-30" % "2.5.0"
   )
